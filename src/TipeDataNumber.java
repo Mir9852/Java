530cb3233652c79fa78bdf451a1531cfc02b5f95
Mir9852 <muh52amir@gmail.com>
2025-09-12 21:23:04 +0700

    add new file

---
+++ src/TipeDataNumber.java
@@ -0,0 +1,20 @@
+public class TipeDataNumber {
+    public static void main(String[] args) {
+        
+        byte iniByte = 100;
+        short iniShort = 10000;
+        int iniInt = 100000;
+        long iniLong = 1000000000;
+        long iniLong2 = 1000000000L;
+
+        float iniFloat = 10.10F;
+        double iniDouble = 10.10;
+
+        int decimalInt=30;
+        int hexDecimal =0xAAAA;
+        int binaryDecimal = 0b101010;
+
+        long amount = 1_000_000_000;
+        
+    }
+}
