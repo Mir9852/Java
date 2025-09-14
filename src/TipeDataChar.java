df3cbd03f642a9afac604fb9d59c406a7a48fafa
Mir9852 <muh52amir@gmail.com>
2025-09-14 15:45:56 +0700

    next file

---
+++ src/TipeDataChar.java
@@ -0,0 +1,14 @@
+public class TipeDataChar {
+    public static void main(String[] args) {
+        
+        char a = 'A';
+        char m = 'M';
+        char i = 'I';
+        char r = 'R';
+
+        System.out.print(a);
+        System.out.print(m);
+        System.out.print(i);
+        System.out.print(r);
+    }
+}
---
+++ src/TipeDataString.java
@@ -0,0 +1,12 @@
+public class TipeDataString {
+    public static void main(String[] args) {
+        
+        String FirstName = "Muhammad";
+        String LastName = "Amir";
+        String FullName = FirstName + " " + LastName;
+        
+        System.out.println(FirstName);
+        System.out.println(LastName);
+        System.out.println(FullName);
+    }
+}
