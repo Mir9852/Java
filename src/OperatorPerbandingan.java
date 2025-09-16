fd89fe855a0cd84f21c76f3da914e67f23d8fc69
Mir9852 <muh52amir@gmail.com>
2025-09-16 20:54:31 +0700

    update !

---
+++ src/OperatorPerbandingan.java
@@ -0,0 +1,14 @@
+public class OperatorPerbandingan {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 100;
+
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+        System.out.println(a == b);
+        System.out.println(a != b);
+
+    }
+}
