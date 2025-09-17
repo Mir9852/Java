3892f1b3173c63c2c7f2ffb12e9b146ff7a4037b
Mir9852 <muh52amir@gmail.com>
2025-09-17 20:57:00 +0700

    lanjut

---
+++ src/Block.java
@@ -0,0 +1,12 @@
+public class Block {
+    public static void main(String[] args) {
+        System.err.println("Hello World");
+        System.out.println("Hai Dunia");
+        System.out.println("Selamat Belajar Java");
+
+        {
+            System.out.println("Ini adalah block");
+            System.out.println("Block digunakan untuk mengelompokkan beberapa statement");
+        }
+    }
+}
